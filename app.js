2ee4b298e01636c95a089f8341d6f0a1af25138f
Ramón Díaz <jramondiaz96@gmail.com>
2024-04-17 00:04:19 -0600

    Se descarga el nombre del archivo con fecha y hora

--- app.js
+++ app.js
@@ -20,19 +20,31 @@ function descargarRespaldo() {
   var request = indexedDB.open("reservacionesDB", 1);
 
   // Manejar el evento de éxito
-  request.onsuccess = function(event) {
+  request.onsuccess = function (event) {
     var db = event.target.result;
-    
+
     // Obtener todas las reservaciones de la base de datos
     var transaction = db.transaction(["reservaciones"], "readonly");
     var reservaStore = transaction.objectStore("reservaciones");
     var request = reservaStore.getAll();
 
-    request.onsuccess = function(event) {
+    request.onsuccess = function (event) {
       var reservaciones = event.target.result;
 
       // Convertir las reservaciones a formato JSON
       var jsonReservaciones = JSON.stringify(reservaciones, null, 2);
+
+      // Obtener la fecha y hora actual
+      var fechaHoraActual = new Date();
+
+      // Formatear la fecha en formato dd-mm-yyyy
+      var fechaFormateada = fechaHoraActual.toLocaleDateString('es-ES', {day: '2-digit', month: '2-digit', year: 'numeric'});
+
+      // Formatear la hora en formato hh-mm-ss
+      var horaFormateada = fechaHoraActual.toLocaleTimeString('es-ES', {hour: '2-digit', minute: '2-digit', second: '2-digit'});
+
+      // Concatenar fecha y hora en el nombre del archivo
+      var nombreArchivo = "respaldo_reservaciones_" + fechaFormateada + "_" + horaFormateada + ".json";
 
       // Crear un objeto Blob con el contenido JSON
       var blob = new Blob([jsonReservaciones], { type: "application/json" });
@@ -43,7 +55,7 @@ function descargarRespaldo() {
       // Crear un elemento de enlace para descargar el archivo
       var a = document.createElement("a");
       a.href = url;
-      a.download = "respaldo_reservaciones.json";
+      a.download = nombreArchivo; // Utilizar el nombre de archivo modificado
 
       // Simular un clic en el enlace para iniciar la descarga
       document.body.appendChild(a);
@@ -55,19 +67,22 @@ function descargarRespaldo() {
   };
 }
 
-// Función para eliminar todas las reservaciones de la base de datos
+
 function eliminarTodasLasReservaciones() {
   var transaction = db.transaction(["reservaciones"], "readwrite");
   var reservaStore = transaction.objectStore("reservaciones");
 
   var request = reservaStore.clear();
 
-  request.onsuccess = function(event) {
+  request.onsuccess = function (event) {
     console.log("Todas las reservaciones eliminadas");
   };
 
-  request.onerror = function(event) {
-    console.error("Error al eliminar todas las reservaciones:", event.target.error);
+  request.onerror = function (event) {
+    console.error(
+      "Error al eliminar todas las reservaciones:",
+      event.target.error
+    );
   };
 }
 
@@ -97,21 +112,23 @@ function agregarReservaciones(reservaciones) {
   var transaction = db.transaction(["reservaciones"], "readwrite");
   var reservaStore = transaction.objectStore("reservaciones");
 
-  reservaciones.forEach(function(reserva) {
+  reservaciones.forEach(function (reserva) {
     reservaStore.add(reserva);
   });
 
-  transaction.oncomplete = function(event) {
+  transaction.oncomplete = function (event) {
     console.log("Reservaciones del respaldo agregadas exitosamente");
     // Mostrar las reservaciones del respaldo en el frontend si es necesario
     mostrarReservaciones(reservaciones);
   };
 
-  transaction.onerror = function(event) {
-    console.error("Error al agregar reservaciones del respaldo:", event.target.error);
+  transaction.onerror = function (event) {
+    console.error(
+      "Error al agregar reservaciones del respaldo:",
+      event.target.error
+    );
   };
 }
-
 
 function obtenerHoraActual() {
   var ahora = new Date();
@@ -127,23 +144,23 @@ function obtenerHoraActual() {
   return hora + ":" + minutos + ":" + segundos;
 }
 
-// Escuchar el evento click en el botón de cargar respaldo
+var archivoSeleccionado;
+
+document
+  .getElementById("input-archivo")
+  .addEventListener("change", function (event) {
+    archivoSeleccionado = event.target.files[0];
+  });
+
 document
   .getElementById("boton-cargar-respaldo")
   .addEventListener("click", function () {
-    var inputArchivo = document.getElementById("input-archivo");
-
-    // Verificar si se seleccionó un archivo
-    if (
-      inputArchivo.files.length === 0 ||
-      !inputArchivo.files[0].name.endsWith(".json")
-    ) {
+    if (!archivoSeleccionado) {
       alert("Por favor, seleccione un archivo JSON.");
       return;
     }
 
-    var archivo = inputArchivo.files[0];
-    cargarRespaldo(archivo);
+    cargarRespaldo(archivoSeleccionado);
   });
 
 function cargarRespaldo(blob) {
@@ -154,27 +171,24 @@ function cargarRespaldo(blob) {
     console.log("Respaldo cargado:", respaldoCargado);
 
     try {
-      // Intenta convertir el respaldo a un objeto o array JSON
       var reservaciones = JSON.parse(respaldoCargado);
 
-      // Verifica si es un array
-      if (Array.isArray(reservaciones)) {
-        // Abre una transacción de lectura/escritura en la base de datos
-        var transaction = db.transaction(["reservaciones"], "readwrite");
-        var reservaStore = transaction.objectStore("reservaciones");
+      // Abre una transacción de lectura/escritura en la base de datos
+      var transaction = db.transaction(["reservaciones"], "readwrite");
+      var reservaStore = transaction.objectStore("reservaciones");
 
-        // Borra todas las reservaciones existentes en la base de datos
-        reservaStore.clear();
+      // Borra todas las reservaciones existentes en la base de datos
+      reservaStore.clear();
 
-        // Agrega las reservaciones del respaldo a la base de datos
-        reservaciones.forEach(function (reserva) {
-          reservaStore.add(reserva);
-        });
+      // Agrega las reservaciones del respaldo a la base de datos
+      reservaciones.forEach(function (reserva) {
+        reservaStore.add(reserva);
+      });
 
-        console.log("Respaldo cargado y añadido a la base de datos.");
-      } else {
-        console.error("El respaldo no es un array.");
-      }
+      console.log("Respaldo cargado y añadido a la base de datos.");
+
+      // Recargar la página después de cargar el respaldo
+      location.reload();
     } catch (error) {
       console.error("Error al procesar el archivo JSON:", error);
     }
@@ -183,21 +197,19 @@ function cargarRespaldo(blob) {
   reader.readAsText(blob);
 }
 
-document
-  .getElementById("input-archivo")
-  .addEventListener("change", function (event) {
-    var archivo = event.target.files[0];
-    cargarRespaldo(archivo);
-  });
 
-
- // dates.js
-
-// Función para mostrar las reservaciones en el frontend
 function mostrarReservaciones(reservaciones) {
   var reservacionesHtml = "<h2>Reservaciones</h2>";
-  reservaciones.forEach(function(reserva) {
-    reservacionesHtml += "<p>Mesa: " + reserva.mesa + ", Hora: " + reserva.hora + ", Usuario: " + reserva.usuario + "</p>";
+  reservaciones.forEach(function (reserva) {
+    reservacionesHtml +=
+      "<p>Mesa: " +
+      reserva.mesa +
+      ", Hora: " +
+      reserva.hora +
+      ", Usuario: " +
+      reserva.usuario +
+      "</p>";
   });
-  document.getElementById("reservaciones-container").innerHTML = reservacionesHtml;
+  document.getElementById("reservaciones-container").innerHTML =
+    reservacionesHtml;
 }
